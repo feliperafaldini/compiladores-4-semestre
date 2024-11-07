badb6b1d78e59aa58de925673feeca7b4b003d79
feliperafaldini <feliperafaldini@gmail.com>
2024-11-07 16:36:17 -0300

    Update

--- Projeto/lexer.py
+++ Projeto/lexer.py
@@ -1,72 +1,93 @@
+import os
 import ply.lex as lex
+
 
 class Lexer(object):
     # Lista de tokens
     tokens = [
-        'ID',        # Identificadores
-        'NUMBER',    # Números
-        'PLUS',      # +
-        'MINUS',     # -
-        'TIMES',     # *
-        'DIVIDE',    # /
-        'LPAREN',    # (
-        'RPAREN',    # )
-        'ASSIGN',    # =
-        'SEMICOLON', # ;
+        "ID",  # Identificadores
+        "NUMBER",  # Números
+        "PLUS",  # +
+        "MINUS",  # -
+        "TIMES",  # *
+        "DIVIDE",  # /
+        "LPAREN",  # (
+        "RPAREN",  # )
+        "LBRACE",  # {
+        "RBRACE",  # }
+        "EQUAL",  # ==
+        "ASSIGN",  # =
+        "COLON",  # :
+        "SEMICOLON",  # ;
+        "LESS",  # <
+        "MORE",  # >
     ]
-    
+
     # Palavras-chave
     keywords = {
-        'if' : 'IF',
-        'else': 'ELSE',
-        'elif': 'ELIF',
-        'do': 'DO',
-        'while': 'WHILE',
-        'print': 'PRINT',
+        "if": "IF",
+        "else": "ELSE",
+        "elif": "ELIF",
+        "do": "DO",
+        "while": "WHILE",
+        "print": "PRINT",
     }
-    
+
     # Adiciona palavras-chave à lista de tokens
     tokens = tokens + list(keywords.values())
-    
+
     # Expressões regulares para tokens
-    t_PLUS = r'\+'
-    t_MINUS = r'-'
-    t_TIMES = r'\*'
-    t_DIVIDE = r'/'
-    t_LPAREN = r'\('
-    t_RPAREN = r'\)'
-    t_ASSIGN = r'='
-    t_SEMICOLON = r';'
-    
+    t_PLUS = r"\+"
+    t_MINUS = r"-"
+    t_TIMES = r"\*"
+    t_DIVIDE = r"/"
+    t_LPAREN = r"\("
+    t_RPAREN = r"\)"
+    t_LBRACE = r"\{"
+    t_RBRACE = r"\}"
+    t_COLON = r":"
+    t_SEMICOLON = r";"
+    t_LESS = r"<"
+    t_MORE = r">"
+    t_EQUAL = r"=="
+    t_ASSIGN = r"="
+
     # Função para identificadores e palavras-chave
     def t_ID(self, t):
-        r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.type = self.keywords.get(t.value, 'ID')  # Define o tipo de token como palavra-chave ou ID
+        r"[a-zA-Z_][a-zA-Z_0-9]*"
+        t.type = self.keywords.get(
+            t.value, "ID"
+        )  # Define o tipo de token como palavra-chave ou ID
         return t
-    
+
     # Função para números
     def t_NUMBER(self, t):
-        r'\d+'
+        r"\d+"
         t.value = int(t.value)  # Converte o valor para inteiro
         return t
-    
+
     # Função para comentários
     def t_COMMENT(self, t):
-        r'\#.*'
+        r"\#.*"
         pass
-        
+
     # Ignorar espaços e tabulações
-    t_ignore = ' \t'
-    
+    t_ignore = " \t"
+
+    # Quebra de linhas
+    def t_newline(self, t):
+        r"\n+"
+        t.lexer.lineno += len(t.value)
+
     # Função para erros léxicos
     def t_error(self, t):
-        print(f"Caractere ilegal '{t.value[0]}'")
+        print(f"Caractere ilegal '{t.value[0]}' na linha {t.lexer.lineno}")
         t.lexer.skip(1)
 
     # Construção do lexer
     def build(self, **kwargs):
         self.lexer = lex.lex(module=self, **kwargs)
-        
+
     # Teste
     def test(self, data):
         self.lexer.input(data)
@@ -76,6 +97,8 @@ class Lexer(object):
                 break
             print(tok)
 
-lexer = Lexer()
-lexer.build()
-lexer.test("print(3 + 4)")
+
+if __name__ == "__main__":
+    lexer = Lexer()
+    lexer.build()
+    lexer.test("""x=1; do {x=x+1;} while (x<3)""")
--- Projeto/parser.py
+++ Projeto/parser.py
@@ -1,2 +1,115 @@
 import ply.yacc as yacc
 from lexer import Lexer
+
+
+class Parser(object):
+    # Importação dos tokens
+    tokens = Lexer.tokens
+
+    # Precedência dos operadores
+    precedence = (
+        ("left", "PLUS", "MINUS"),
+        ("left", "TIMES", "DIVIDE"),
+        ("left", "LESS", "MORE", "EQUAL"),
+    )
+
+    # Regras gramaticais
+    def p_statement_assign(self, p):
+        "statement : ID ASSIGN expression SEMICOLON"
+        p[0] = ("assign", p[1], p[3])
+
+    def p_statement_expr(self, p):
+        "statement : expression SEMICOLON"
+        p[0] = ("expr", p[1])
+
+    def p_expression_binop(self, p):
+        """expression : expression PLUS expression
+        | expression MINUS expression
+        | expression TIMES expression
+        | expression DIVIDE expression
+        | expression LESS expression
+        | expression MORE expression
+        | expression EQUAL expression"""
+        p[0] = (p[2], p[1], p[3])
+
+    def p_expression_group(self, p):
+        "expression : LPAREN expression RPAREN"
+        p[0] = p[2]
+
+    def p_expression_number(self, p):
+        "expression : NUMBER"
+        p[0] = p[1]
+
+    def p_expression_id(self, p):
+        "expression : ID"
+        p[0] = ("id", p[1])
+
+    def p_statement_do(self, p):
+        """statement : DO LBRACE statement_list RBRACE"""
+        p[0] = ("do", p[3])
+
+    def p_statement_do_while(self, p):
+        """statement : DO LBRACE statement_list RBRACE WHILE LPAREN expression RPAREN SEMICOLON"""
+        p[0] = ("do_while", p[3], p[7])
+
+    def p_statement_list(self, p):
+        """statement_list : statement
+        | statement statement_list"""
+        if len(p) == 2:
+            p[0] = [p[1]]
+        else:
+            p[0] = [p[1]] + p[2]
+
+    def p_statement_print(self, p):
+        "statement : PRINT LPAREN expression RPAREN"
+        p[0] = ("print", p[3])
+
+    def p_statement_if(self, p):
+        """statement : IF LPAREN expression RPAREN LBRACE statement RBRACE
+        | IF LPAREN expression RPAREN RPAREN statement RBRACE ELIF LPAREN expression RPAREN RPAREN statement RBRACE
+        | IF LPAREN expression RPAREN RPAREN statement RBRACE ELSE RPAREN statement RBRACE
+        | IF LPAREN expression RPAREN RPAREN statement RBRACE ELIF LPAREN expression RPAREN RPAREN statement RBRACE ELSE COLON statement RBRACE
+        """
+
+        if len(p) == 6:
+            p[0] = ("if", p[3], p[5], None, None)
+        elif len(p) == 10:
+            p[0] = ("if", p[3], p[5], ("elif", p[7], p[9]), None)
+        elif len(p) == 12:
+            p[0] = ("if", p[3], p[5], ("elif", p[7], p[9]), p[11])
+        elif len(p) == 14:
+            p[0] = (
+                "if",
+                p[3],
+                p[5],
+                [("elif", p[7], p[9]), ("elif", p[11], p[13])],
+                p[15],
+            )
+
+    # Tratamento de erros sintáticos
+    def p_error(self, p):
+        if p:
+            print(
+                f"Erro sintático encontrado: Token {p.type}, com o valor '{p.value}' na linha {p.lineno}"
+            )
+        else:
+            print(f"Erro sintático encontrado: Erro na entrada")
+
+    # Construção do parser
+    def build(self):
+        self.parser = yacc.yacc(module=self)
+
+    # Teste do parser
+    def test(self, data):
+        lexer = Lexer()
+        lexer.build()
+        lexer.lexer.input(data)
+        result = self.parser.parse(data, lexer=lexer.lexer)
+        print(result)
+
+
+if __name__ == "__main__":
+    parser = Parser()
+    parser.build()
+    data = """x = 1; do {x=x+1;} while (1+1);"""
+    parser.test(data)
